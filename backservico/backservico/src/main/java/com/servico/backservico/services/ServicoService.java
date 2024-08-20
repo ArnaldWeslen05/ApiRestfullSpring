40d029ab5cec53c0f11d1b728d9153153865d611
ArnaldWeslen05 <arnaldclash@gmail.com>
2024-08-20 00:44:21 -0300

    Create Service

---
+++ backservico/backservico/src/main/java/com/servico/backservico/services/ServicoService.java
@@ -0,0 +1,44 @@
+package com.servico.backservico.services;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.servico.backservico.models.Servico;
+import com.servico.backservico.respository.ServicoRepository;
+
+@Service
+public class ServicoService {
+    @Autowired
+    private ServicoRepository servicoRepository;
+
+    // para listar todos os serviços buscados pelo cliente
+    public List<Servico> buscarTodos(){
+        return servicoRepository.findAll();
+    }
+
+    public Servico inserir( Servico servico){
+        if(servico.getValorPago()==null || servico.getValorPago()==0 || servico.getDataPagamento()==null){
+            servico.setStatus("pendente");
+        }else{
+            servico.setStatus("Realizado");
+        }
+        Servico servicoBanco = servicoRepository.save(servico);
+        return servicoBanco;
+    }
+
+    public Servico alterar(Servico servico){
+        if(servico.getValorPago()!= null && servico.getValorPago()>0 && servico.getDataPagamento()!=null){
+            servico.setStatus("Realizado");
+        }
+
+
+        return servicoRepository.saveAndFlush(servico);
+    }
+
+    public void eexcluir (Long id){
+        Servico servico = servicoRepository.findById(id).get();
+        servicoRepository.delete(servico);
+    }
+}
