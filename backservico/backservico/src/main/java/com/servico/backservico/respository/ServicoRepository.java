b514f14658370d4566a2bf7563c4a77a8b368177
ArnaldWeslen05 <arnaldclash@gmail.com>
2024-08-20 00:55:32 -0300

    Query for pagamentos pendente and cancelados

--- backservico/backservico/src/main/java/com/servico/backservico/respository/ServicoRepository.java
+++ backservico/backservico/src/main/java/com/servico/backservico/respository/ServicoRepository.java
@@ -1,11 +1,18 @@
 package com.servico.backservico.respository;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 import com.servico.backservico.models.Servico;
 
 public interface ServicoRepository extends JpaRepository<Servico,Long>{
 
-    
+    @Query("select s from Servico s where s.valorPago <> null and s.valorPago >0")
+    List<Servico> buscarServicosPagamentoPendente(); 
+
+    @Query("select s from Servico s where s.status = 'cancelado' ")
+    List<Servico> buscarServicosCancelados(); 
     
 }
