15e61ed9d2995061044e9100a477ef310dc4ed3f
ArnaldWeslen05 <arnaldclash@gmail.com>
2024-08-21 01:39:00 -0300

    create cancelar serviço

--- backservico/backservico/src/main/java/com/servico/backservico/controller/ServicoController.java
+++ backservico/backservico/src/main/java/com/servico/backservico/controller/ServicoController.java
@@ -6,6 +6,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -15,7 +16,6 @@ import org.springframework.web.bind.annotation.RestController;
 import com.servico.backservico.models.Servico;
 import com.servico.backservico.services.ServicoService;
 
-import jakarta.websocket.server.PathParam;
 
 
 @RestController
@@ -42,6 +42,11 @@ public class ServicoController {
     public Servico inserir(@RequestBody Servico servico){
         return servicoService.inserir(servico);
     }
+    @PostMapping("/{id}")
+    public ResponseEntity<Void> cancelar(@PathVariable("id") Long id){
+        servicoService.cancelarServico(id);
+        return ResponseEntity.ok().build();
+    }
 
     @PutMapping("/")
     public Servico alterar(@RequestBody Servico servico){
@@ -49,7 +54,7 @@ public class ServicoController {
     }
 
     @DeleteMapping("/{id}")
-    public ResponseEntity<Void> remover(@PathParam("id")Long id){
+    public ResponseEntity<Void> remover(@PathVariable("id")Long id){
         servicoService.excluir(id);
         return ResponseEntity.ok().build();
     }
